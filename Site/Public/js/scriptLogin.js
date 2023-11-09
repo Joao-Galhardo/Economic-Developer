b60653bdf049979781f1b118c9f94e6cea610795
Joao-Galhardo <joao.galhardo@sptech.school>
2023-11-08 23:32:57 -0300

    Criando as duas paginas de apoio

--- Site/Public/js/scriptLogin.js
+++ Site/Public/js/scriptLogin.js
@@ -1,8 +1,17 @@
-
-const loginContainer = document.getElementById('login-container') // obtenho a referencia do HTML pelo id //
-
-const moveOverlay = () => loginContainer.classList.toggle('move') // Crio uma const que tem como função utilizar o método toggle que representa a lista de classes de um elemento, ele adicionar a classe move ou remove //
+const moveOverlay = () => loginContainer.classList.toggle('move')
 
 document.getElementById('abrir-cadastrar').addEventListener('click', moveOverlay) // Adiciono um event listener, toda vez que eu clicar ele vai chamar aquela função 
 document.getElementById('abrir-login').addEventListener('click', moveOverlay)
+
+document.addEventListener("DOMContentLoaded", function () {
+    var parametro = window.location.search;
+    console.log(parametro)
+
+    if (parametro == "?login=true") {
+        document.getElementById('abrir-login').click();
+    }
+})
+
+const loginContainer = document.getElementById('login-container') // obtenho a referencia do HTML pelo id //
+ // Crio uma const que tem como função utilizar o método toggle que representa a lista de classes de um elemento, ele adicionar a classe move ou remove //
 
